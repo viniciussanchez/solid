eba3b0d4311f19310141090e851791a0dc2c9777
viniciussanchez <viniciuss.sanchez@gmail.com>
2021-07-13 09:45:15 -0300

    Adicionado novo exemplo de LSP

--- lsp/LSP.dpr
+++ lsp/LSP.dpr
@@ -3,13 +3,19 @@ program LSP;
 uses
   Vcl.Forms,
   LSP.Main in 'src\LSP.Main.pas' {FrmExemploLSP},
-  LSP.Errado.Conta in 'src\errado\LSP.Errado.Conta.pas',
-  LSP.Errado.Conta.Salario in 'src\errado\LSP.Errado.Conta.Salario.pas',
-  LSP.Certo.Conta in 'src\certo\LSP.Certo.Conta.pas',
-  LSP.Certo.Conta.Bancaria in 'src\certo\LSP.Certo.Conta.Bancaria.pas',
-  LSP.Certo.Conta.Salario in 'src\certo\LSP.Certo.Conta.Salario.pas',
-  LSP.Errado.Conta.Poupanca in 'src\errado\LSP.Errado.Conta.Poupanca.pas',
-  LSP.Certo.Conta.Poupanca in 'src\certo\LSP.Certo.Conta.Poupanca.pas';
+  LSP.Certo.Conta.Bancaria in 'src\certo\conta\LSP.Certo.Conta.Bancaria.pas',
+  LSP.Certo.Conta in 'src\certo\conta\LSP.Certo.Conta.pas',
+  LSP.Certo.Conta.Poupanca in 'src\certo\conta\LSP.Certo.Conta.Poupanca.pas',
+  LSP.Certo.Conta.Salario in 'src\certo\conta\LSP.Certo.Conta.Salario.pas',
+  LSP.Errado.Conta in 'src\errado\conta\LSP.Errado.Conta.pas',
+  LSP.Errado.Conta.Poupanca in 'src\errado\conta\LSP.Errado.Conta.Poupanca.pas',
+  LSP.Errado.Conta.Salario in 'src\errado\conta\LSP.Errado.Conta.Salario.pas',
+  LSP.Errado.Ave in 'src\errado\ave\LSP.Errado.Ave.pas',
+  LSP.Errado.Tucano in 'src\errado\ave\LSP.Errado.Tucano.pas',
+  LSP.Errado.Pinguin in 'src\errado\ave\LSP.Errado.Pinguin.pas',
+  LSP.Certo.Ave in 'src\certo\ave\LSP.Certo.Ave.pas',
+  LSP.Certo.Tucano in 'src\certo\ave\LSP.Certo.Tucano.pas',
+  LSP.Certo.Pinguin in 'src\certo\ave\LSP.Certo.Pinguin.pas';
 
 {$R *.res}
 
--- lsp/src/LSP.Main.pas
+++ lsp/src/LSP.Main.pas
@@ -20,36 +20,37 @@ implementation
 
 {$R *.dfm}
 
-uses LSP.Errado.Conta, LSP.Errado.Conta.Salario, System.Generics.Collections, LSP.Certo.Conta, LSP.Certo.Conta.Bancaria,
-  LSP.Certo.Conta.Salario, LSP.Errado.Conta.Poupanca, LSP.Certo.Conta.Poupanca;
+uses System.Generics.Collections,
+  LSP.Errado.Conta, LSP.Errado.Conta.Poupanca, LSP.Errado.Conta.Salario;
+//  LSP.Certo.Conta, LSP.Certo.Conta.Bancaria, LSP.Certo.Conta.Salario, LSP.Certo.Conta.Poupanca;
 
 procedure TFrmExemploLSP.btnPercorrerContasCertoClick(Sender: TObject);
-var
-  LConta: LSP.Certo.Conta.Bancaria.TContaBancaria;
-  LContas: TObjectList<LSP.Certo.Conta.Bancaria.TContaBancaria>;
+//var
+//  LConta: TContaBancaria;
+//  LContas: TObjectList<TContaBancaria>;
 begin
-  LContas := TObjectList<LSP.Certo.Conta.Bancaria.TContaBancaria>.Create();
-  try
-    LContas.Add(LSP.Certo.Conta.Bancaria.TContaBancaria.Create);
-    LContas.Add(LSP.Certo.Conta.Poupanca.TContaPoupanca.Create);
-    //LContas.Add(LSP.Certo.Conta.Salario.TContaSalario.Create);
-    for LConta in LContas do
-      LConta.Render;
-  finally
-    LContas.Free;
-  end;
+//  LContas := TObjectList<TContaBancaria>.Create();
+//  try
+//    LContas.Add(TContaBancaria.Create);
+//    LContas.Add(TContaPoupanca.Create);
+//    //LContas.Add(TContaSalario.Create);
+//    for LConta in LContas do
+//      LConta.Render;
+//  finally
+//    LContas.Free;
+//  end;
 end;
 
 procedure TFrmExemploLSP.btnPercorrerContasErradoClick(Sender: TObject);
 var
-  LConta: LSP.Errado.Conta.TContaBancaria;
-  LContas: TObjectList<LSP.Errado.Conta.TContaBancaria>;
+  LConta: TContaBancaria;
+  LContas: TObjectList<TContaBancaria>;
 begin
-  LContas := TObjectList<LSP.Errado.Conta.TContaBancaria>.Create();
+  LContas := TObjectList<TContaBancaria>.Create();
   try
-    LContas.Add(LSP.Errado.Conta.TContaBancaria.Create);
-    LContas.Add(LSP.Errado.Conta.Poupanca.TContaPoupanca.Create);
-    LContas.Add(LSP.Errado.Conta.Salario.TContaSalario.Create);
+    LContas.Add(TContaBancaria.Create);
+    LContas.Add(TContaPoupanca.Create);
+    LContas.Add(TContaSalario.Create);
     for LConta in LContas do
       LConta.Render;
   finally
